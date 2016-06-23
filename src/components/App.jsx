f8fb47d836a54d59c5b9a47394579a4d389fdaee
syed-niotic <syed.ahmed@niotic.com>
2016-06-23 18:24:32 -0400

    created skeleton of the app

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, {component} from 'react';
+import ReactDOM from 'react-DOM';
+
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+  render(){
+    return(
+      <div> This is my app </div>
+    )
+  }
+}
+
+export default App
